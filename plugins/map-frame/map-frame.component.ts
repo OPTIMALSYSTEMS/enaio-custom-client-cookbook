76ae34037df792c4e56004854d46873544d9b59d
fwille <f.wille@optimal-systems.de>
2018-08-15 16:30:34 +0200

    parseDmsParams to get DmsObject by Params

--- plugins/map-frame/map-frame.component.ts
+++ plugins/map-frame/map-frame.component.ts
@@ -1,25 +1,17 @@
-import {
-  Component,
-  OnInit,
-  ViewChild,
-  ElementRef,
-  Renderer2,
-  AfterViewInit
-} from '@angular/core';
-import { SelectionService } from 'eo-client/src/app/eo-framework-core/selection/selection.service';
-import { DmsService } from 'eo-client/src/app/eo-framework-core/dms/dms.service';
-import { DmsObject } from 'eo-client/src/app/eo-framework-core/model/dms-object.model';
+import { Component, AfterViewInit, ViewChild, ElementRef, Renderer2 } from '@angular/core';
 import { EnaioEvent } from 'eo-client/src/app/eo-framework-core/events/events';
+import { DmsObject } from 'eo-client/src/app/eo-framework-core/model/dms-object.model';
+import { DmsService, DmsParams } from 'eo-client/src/app/eo-framework-core/dms/dms.service';
+import { SelectionService } from 'eo-client/src/app/eo-framework-core/selection/selection.service';
 import { EventService } from 'eo-client/src/app/eo-framework-core/events/event.service';
-
-import { ActivatedRoute } from '@angular/router';
+import { filter, flatMap, map } from 'rxjs/operators';
 
 @Component({
   selector: 'eo-map-frame',
   templateUrl: './map-frame.component.html',
   styleUrls: ['./map-frame.component.scss']
 })
-export class MapFrameComponent implements OnInit, AfterViewInit {
+export class MapFrameComponent implements AfterViewInit {
   static id = 'eo.custom.plugin.map-frame';
   static matchType: RegExp = /object-details-tab.*/;
 
@@ -32,46 +24,43 @@ export class MapFrameComponent implements OnInit, AfterViewInit {
     private selectionService: SelectionService,
     private dmsService: DmsService,
     private renderer: Renderer2,
-    private eventService: EventService,
-    private route: ActivatedRoute
-  ) {
-    this.route.params.subscribe(routeParams => {
-      this.routeId = routeParams.id;
-    });
+    private eventService: EventService
+  ) {}
+
+  private parseDmsParams(data: any): DmsParams {
+    let dms: DmsParams;
+    if (data.hasOwnProperty('favoritetype')) {
+      dms = data && data.target ? {id: data.target.id, type: data.target.type} : null;
+    } else {
+      dms = data ? {...data, type: data.typeName || data.type} : null;
+    }
+    return dms;
   }
 
-  setup(data) {
+  private renderMap(address = '', city = '', country = '') {
+    const url = `https://www.google.com/maps/embed/v1/place?key=AIzaSyDX8znfh-d4u3spGhC1GvCjq6EA1pjPovQ&q=${address}+${city}+${country}`;
+    this.renderer.setAttribute(this.mapFrame.nativeElement, 'src', url);
+  }
+
+  setupMap(data) {
+    const { strassehw, orthw, landhw } = data.data;
     this.context = data;
-    this.renderer.setAttribute(
-      this.mapFrame.nativeElement,
-      'src',
-      `https://www.google.com/maps/embed/v1/place?key=AIzaSyDX8znfh-d4u3spGhC1GvCjq6EA1pjPovQ&q=${data.data.address}+${data.data.city}+${data.data.country}`
-    );
+    this.renderMap(strassehw, orthw, landhw);
   }
 
   ngAfterViewInit() {
-    if (this.routeId) {
-      this.dmsService
-        .getDmsObject(this.routeId)
-        .subscribe((data: DmsObject) => {
-          this.setup(data);
-        });
-    }
-
     this.selectionService.focus$
-      .filter(d => d.id)
-      .flatMap(d => this.dmsService.getDmsObjectByParams(d))
-      .subscribe((data: DmsObject) => {
-        this.setup(data);
-      });
+      .pipe(
+        map(d => d.target || d.dmsItem || d),
+        filter(d => d.id),
+        flatMap(d => this.dmsService.getDmsObjectByParams(this.parseDmsParams(d) as DmsParams))
+      )
+      .subscribe((data: DmsObject) => this.setupMap(data), error => this.renderMap());
 
     this.eventService.on(EnaioEvent.DMS_OBJECT_UPDATED).subscribe(event => {
       if (this.context && this.context.id === event.data.id) {
-        this.setup(event.data);
+        this.setupMap(event.data);
       }
     });
   }
-
-  ngOnInit() {}
 }
-
